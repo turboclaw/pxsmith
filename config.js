48584ea707d56601aadd3bf187d189b095f7c727
Dan Nixon <dan@turboclaw.com>
2015-11-23 22:14:28 -0500

    added grid, split css files

--- config.js
+++ config.js
@@ -18,6 +18,7 @@ System.config({
     "babel": "npm:babel-core@5.8.34",
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "core-js": "npm:core-js@1.2.6",
+    "dpz-grid": "github:dominosui/dpz-grid@master",
     "jquery": "github:components/jquery@2.1.4",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
