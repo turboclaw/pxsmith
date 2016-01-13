a9116362bde19398e154ef1356a02e18f459be91
Dan Nixon <dan@turboclaw.com>
2016-01-12 21:53:02 -0500

    wip drawer stuff

--- src/js/views/landing/drawer.js
+++ src/js/views/landing/drawer.js
@@ -7,5 +7,15 @@ export default Marionette.LayoutView.extend({
     template: JST["landing/drawer"],
     className: "drawer",
     regions: {
+    },
+    events: {
+        "click [data-hook='acc-toggle']": "toggleAccordion"
+    },
+    onShow() {
+        $("[data-hook='acc-content']", this.$el).hide();
+    },
+    toggleAccordion(evt) {
+        $("[data-hook='acc-content']", this.$el).slideUp();
+        $(evt.currentTarget).closest(".acc__item").addClass(".is-open").find("[data-hook='acc-content']").slideDown();
     }
 });
